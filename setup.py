80cd283ecc88d3a383201498a657e8c77b4e93c9
dhnchandan <php.chandan@gmail.com>
2024-06-05 19:27:51 +0600

    feat: :sparkles: Add setuptool

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fly_conda_test',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/dhnchandan/fly-conda-test-feedstock.git',
+    license='MIT',
+    author='Dilwar Hossain Noor',
+    author_email='php.chandan@gmail.com',
+    description='Test anaconda package'
+)
