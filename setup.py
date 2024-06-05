8367b0626d708241cfc3b8eea4cdb0a654bf7743
dhnchandan <php.chandan@gmail.com>
2024-06-06 01:34:45 +0600

    feat: :construction_worker: Build with gpu

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='fly_conda_test',
-    version='0.3.0',
+    version='0.4.0',
     packages=find_packages(),
     install_requires=[],
     url='https://github.com/dhnchandan/fly-conda-test-feedstock.git',
     license='MIT',
     author='Dilwar Hossain Noor',
     author_email='php.chandan@gmail.com',
-    description='Test anaconda package'
+    description='Test anaconda package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
 )
