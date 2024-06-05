bf278f5305428f94a03cfd244f10a3581c7f8138
dhnchandan <php.chandan@gmail.com>
2024-06-06 01:34:15 +0600

    feat: :sparkles: Add gpu calculations

--- fly_conda_test/calculator.py
+++ fly_conda_test/calculator.py
@@ -1,17 +1,34 @@
+import numpy as np
+import cupy as cp
+
+
 def add(a, b):
     """Addition function"""
     return a + b
+
 
 def subtract(a, b):
     """Subtraction function"""
     return a - b
 
+
 def multiply(a, b):
     """Multiplication function"""
     return a * b
+
 
 def divide(a, b):
     """Division function"""
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
+
+
+def compute_cpu(a: int, b: int):
+    arr = np.random.rand(a, b)
+    return np.sum(arr ** 2)
+
+
+def compute_gpu(a: int, b: int):
+    arr = cp.random.rand(a, b)
+    return cp.sum(arr ** 2)
