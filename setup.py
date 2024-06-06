f2a057ca464b964be50fb01b376439898f1c270c
dhnchandan <php.chandan@gmail.com>
2024-06-06 17:23:46 +0600

    chore: :construction_worker: Update version to 0.10.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fly_conda_test',
-    version='0.9.0',
+    version='0.10.0',
     packages=find_packages(),
     install_requires=[],
     url='https://github.com/dhnchandan/fly-conda-test-feedstock.git',
