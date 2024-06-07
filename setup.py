efa1bbbb3d39bbe964b4ed3637e59e59e09c04cc
dhnchandan <php.chandan@gmail.com>
2024-06-07 23:49:27 +0600

    refactor: :construction_worker: Update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name='fly_conda_test',
     version='0.12.0',
     packages=find_packages(),
-    include_package_data=True,
     install_requires=[],
     url='https://github.com/dhnchandan/fly-conda-test-feedstock.git',
     license='MIT',
